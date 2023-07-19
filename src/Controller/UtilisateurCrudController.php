8ec2b942488261d39d4b1daf9c9742905a85eecc
2alheure <jojo77.juv@live.fr>
2023-07-19 12:11:10 +0200

    Début CRUD utilisateur

---
+++ src/Controller/UtilisateurCrudController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Utilisateur;
+use App\Form\UtilisateurFormType;
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UtilisateurCrudController extends AbstractController {
+    #[Route('/utilisateur/create', name: 'utilisateur_create')]
+    public function index(Request $request, EntityManagerInterface $em): Response {
+        $utilisateur = new Utilisateur;
+
+        $form = $this->createForm(UtilisateurFormType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->persist($utilisateur);
+            $em->flush();
+
+            return $this->redirectToRoute('home', [
+                'id' => $utilisateur->getId()
+            ]);
+        }
+
+
+        return $this->render('utilisateur_crud/formulaire.html.twig', [
+            'formulaire' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/utilisateur/update/{id}', name: 'utilisateur_update')]
+    public function update(int $id, UtilisateurRepository $ur, Request $request, EntityManagerInterface $em): Response {
+        $utilisateur = $ur->find($id);
+
+        $form = $this->createForm(UtilisateurFormType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->persist($utilisateur);
+            $em->flush();
+
+            return $this->redirectToRoute('home', [
+                'id' => $utilisateur->getId()
+            ]);
+        }
+
+        $this->addFlash('error', 'Tu t\'es gourré');
+
+        return $this->render('utilisateur_crud/formulaire.html.twig', [
+            'formulaire' => $form->createView(),
+        ]);
+    }
+}
